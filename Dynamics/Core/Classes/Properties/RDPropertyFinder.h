afff765b1dd98211bb8967d7e3e8a4b03b0c4271
Alex Denisov <1101.debian@gmail.com>
2013-09-22 19:34:16 +0300

    Finder added

---
+++ Dynamics/Core/Classes/Properties/RDPropertyFinder.h
@@ -0,0 +1,22 @@
+//
+// Created by Alex Denisov on 22.09.13.
+// Copyright (c) 2013 railsware. All rights reserved.
+//
+
+#pragma once
+
+class RDInternalProperty;
+
+class RDPropertyFinder
+{
+private:
+    id _self;
+
+private:
+    NSArray *properties() const;
+
+public:
+    RDPropertyFinder(id self);
+    RDInternalProperty *findByAccessor(SEL cmd) const;
+    RDInternalProperty *findByMutator(SEL cmd) const;
+};
---
+++ Dynamics/Core/Classes/Properties/RDPropertyFinder.mm
@@ -0,0 +1,43 @@
+//
+// Created by Alex Denisov on 22.09.13.
+// Copyright (c) 2013 railsware. All rights reserved.
+//
+
+#include "RDPropertyFinder.h"
+#include "RDProperty_Private.h"
+#include "RDDefinitions.h"
+
+RDPropertyFinder::RDPropertyFinder(id self) : _self(self)
+{
+}
+
+NSArray *RDPropertyFinder::properties() const
+{
+    NSArray *properties = objc_getAssociatedObject([_self class], kCachedPropertiesKey);
+
+    return properties;
+}
+
+RDInternalProperty *RDPropertyFinder::findByAccessor(SEL cmd) const
+{
+    NSString *accessorName = NSStringFromSelector(cmd);
+    for (RDProperty *p in this->properties()) {
+        if ([p.accessor isEqualToString:accessorName]) {
+            return p.internalProperty;
+        }
+    }
+
+    return NULL;
+}
+
+RDInternalProperty *RDPropertyFinder::findByMutator(SEL cmd) const
+{
+    NSString *mutatorName = NSStringFromSelector(cmd);
+    for (RDProperty *p in this->properties()) {
+        if ([p.mutator isEqualToString:mutatorName]) {
+            return p.internalProperty;
+        }
+    }
+
+    return NULL;
+}
