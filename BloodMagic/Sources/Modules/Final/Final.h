ebd2b755b808e6df4520b34f805b83b44e73440e
AlexDenisov <1101.debian@gmail.com>
2014-02-06 18:39:57 +0200

    [#14] 'Final' header added

---
+++ BloodMagic/Sources/Modules/Final/Final.h
@@ -0,0 +1,13 @@
+//
+//  Final.h
+//  BloodMagic
+//
+//  Created by AlexDenisov on 2/6/14.
+//  Copyright (c) 2014 Railsware. All rights reserved.
+//
+
+#import "BMFinal.h"
+
+#ifndef bm_final
+#define bm_final
+#endif
