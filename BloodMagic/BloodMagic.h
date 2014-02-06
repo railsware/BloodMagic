40b8ecf73951a4ecdf4ce676ae042ab5b5b4125a
dodikk <dodikk88.reg@gmail.com>
2014-02-06 23:42:03 +0200

    [static lib] Added an empty project

---
+++ BloodMagic/BloodMagic.h
@@ -0,0 +1,14 @@
+//
+//  BloodMagic.h
+//  BloodMagic
+//
+//  Created by Oleksandr Dodatko on 06/02/2014.
+//  Copyright (c) 2014 railsware. All rights reserved.
+//
+
+#ifndef BloodMagic_BloodMagic_h
+#define BloodMagic_BloodMagic_h
+
+
+
+#endif
---
+++ StaticLibraryProject/BloodMagic/BloodMagicTests/BloodMagicTests.m
@@ -0,0 +1,34 @@
+//
+//  BloodMagicTests.m
+//  BloodMagicTests
+//
+//  Created by Oleksandr Dodatko on 06/02/2014.
+//  Copyright (c) 2014 railsware. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BloodMagicTests : XCTestCase
+
+@end
+
+@implementation BloodMagicTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
