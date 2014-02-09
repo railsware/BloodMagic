74c177043948b89f54d0ad528fcef9a9bfef9b42
dodikk <dodikk88.reg@gmail.com>
2014-02-09 13:50:06 +0200

    Fixed nib processing for OS X
    Unstable ==> Needs testing

--- BloodMagic/Sources/Modules/Partial/Private/BMPartialHook.mm
+++ BloodMagic/Sources/Modules/Partial/Private/BMPartialHook.mm
@@ -7,10 +7,18 @@
 #import "BMProperty.h"
 #import "BMPropertyValueService.h"
 
+
+
 @implementation BMPartialHook
 
 + (void)accessorHook:(id *)value withProperty:(const BMProperty *)property sender:(id)sender
 {
+   NSParameterAssert( NULL != value );
+//   if ( NULL == value )
+//   {
+//      return;
+//   }
+   
     if (*value != nil) {
         return;
     }
@@ -20,8 +28,26 @@
     }
 
     NSString *nibName = NSStringFromClass(property.propertyClass);
+   
+#if TARGET_OS_MAC
+   NSNib *nib = [ [ NSNib alloc ] initWithNibNamed: nibName
+                                            bundle: nil ];
+   
+   NSArray* topLevelObjects = nil;
+   BOOL isNibInstantiated = [ nib instantiateWithOwner: nil
+                                       topLevelObjects: &topLevelObjects ];
+   NSParameterAssert( isNibInstantiated );
+//   if ( !isNibInstantiated )
+//   {
+//      return;
+//   }
+   
+   *value = [ topLevelObjects lastObject ];
+#else
     UINib *nib = [UINib nibWithNibName:nibName bundle:nil];
-    *value = [[nib instantiateWithOwner:nil options:nil] lastObject];
+     *value = [[nib instantiateWithOwner:nil options:nil] lastObject];
+#endif
+
 
     setValueForProperty(sender, property, *value);
 }
