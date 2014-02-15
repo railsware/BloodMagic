05d1865c948ec7d2f9020c911973e24f0a770057
AlexDenisov <1101.debian@gmail.com>
2014-02-15 17:20:47 +0200

    BM_UNAVAILABLE attribute added

--- BloodMagic/Sources/Modules/Core/Public/BMPublicCoreDefnitions.h
+++ BloodMagic/Sources/Modules/Core/Public/BMPublicCoreDefnitions.h
@@ -7,10 +7,22 @@
 
 typedef id (^magic_initializer_t)(id sender);
 
+#ifndef BM_UNAVAILABLE
+
+    #if __has_extension(attribute_unavailable_with_message)
+        #define BM_UNAVAILABLE(MSG) __attribute__((unavailable(MSG)))
+    #elif __has_extension(attribute_unavailable)
+        #define BM_UNAVAILABLE(MSG) __attribute__((unavailable))
+    #else
+        #define BM_UNAVAILABLE(MSG)
+    #endif
+
+#endif
+
 #ifndef register_module
 
 #define register_module(protocol, name) \
 dynamic name; \
-+ (void)protocol ## __ ## name {}
++ (void)protocol ## __ ## name BM_UNAVAILABLE("BloodMagic's empty method") {}
 
 #endif
