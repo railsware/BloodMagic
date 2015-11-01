ac8dbf30562d07f340ae55e6fdbd4a33fd9d0b11
AlexDenisov <1101.debian@gmail.com>
2015-11-01 14:29:57 +0100

    Remove Cedar from repo

--- Specs/Frameworks/Cedar-iOS.framework/Headers/ComparatorsBase.h
+++
@@ -1,6 +0,0 @@
-#import "CompareEqual.h"
-#import "CompareGreaterThan.h"
-
-#if TARGET_OS_IPHONE
-#import "UIGeometryCompareEqual.h"
-#endif
--- Specs/Frameworks/Cedar-iOS.framework/Versions/Current/Headers/CDRFunctions.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-NSArray *CDRReportersFromEnv(const char*defaultReporterClassName);
-
-int runSpecs();
-int runAllSpecs() __attribute__((deprecated("Please use runSpecs()")));
-int runSpecsWithCustomExampleReporters(NSArray *reporters);
-NSArray *specClassesToRun();
